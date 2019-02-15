9f18e9dfa4a826e8a2c2d7843466b0cc547a2a3b
dql_aly <13217504518@163.com>
2019-02-15 17:57:51 +0800

    修改更新

--- src/android/Update.java
+++ src/android/Update.java
@@ -54,21 +54,4 @@ public class Update extends CordovaPlugin {
             return "0.0.0";
         }
     }
-    /**
-     * 获取当前本地apk的版本
-     *
-     * @param mContext
-     * @return
-     */
-    public static int getVersionCode(Context mContext) {
-        int versionCode = 0;
-        try {
-            //获取软件版本号，对应AndroidManifest.xml下android:versionCode
-            versionCode = mContext.getPackageManager().
-                    getPackageInfo(mContext.getPackageName(), 0).versionCode;
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
-        return versionCode;
-    }
 }

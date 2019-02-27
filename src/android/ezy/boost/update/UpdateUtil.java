327be15822ab1a8905efc6dd6516cae4613d09ef
dql_aly <13217504518@163.com>
2019-02-27 09:54:10 +0800

    修改更新

--- src/android/ezy/boost/update/UpdateUtil.java
+++ src/android/ezy/boost/update/UpdateUtil.java
@@ -110,8 +110,59 @@ public class UpdateUtil {
         String md5 = context.getSharedPreferences(PREFS, 0).getString(KEY_UPDATE, "");
         File apk = new File(context.getExternalCacheDir(), md5 + ".apk");
         if (UpdateUtil.verify(apk, md5)) {
-            install(context, apk, force);
+            openAPKFile(context, apk, force);
         }
+    }
+     /**
+      * 打开安装包
+      *
+      * @param mContext
+      * @param fileUri
+      */
+      public void openAPKFile(Context context, File file, boolean force) {
+        DataEmbeddingUtil.dataEmbeddingAPPUpdate(fileUri);
+        // 核心是下面几句代码
+        if (null != fileUri) {
+          try {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            File apkFile = new File(fileUri);
+            //兼容7.0
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+              intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+              Uri contentUri = FileProvider.getUriForFile(mContext, mContext.getPackageName() + ".fileProvider", apkFile);
+              intent.setDataAndType(contentUri, "application/vnd.android.package-archive");
+              //兼容8.0
+              if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                boolean hasInstallPermission = mContext.getPackageManager().canRequestPackageInstalls();
+                if (!hasInstallPermission) {
+                  ToastUtil.makeText(MyApplication.getContext(), MyApplication.getContext().getString(R.string.string_install_unknow_apk_note), false);
+                  startInstallPermissionSettingActivity();
+                  return;
+                }
+              }
+            } else {
+              intent.setDataAndType(Uri.fromFile(apkFile), "application/vnd.android.package-archive");
+              intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            }
+            if (mContext.getPackageManager().queryIntentActivities(intent, 0).size() > 0) {
+              mContext.startActivity(intent);
+            }
+          } catch (Throwable e) {
+            e.printStackTrace();
+            DataEmbeddingUtil.dataEmbeddingAPPUpdate(e.toString());
+            CommonUtils.makeEventToast(MyApplication.getContext(), MyApplication.getContext().getString(R.string.download_hint), false);
+          }
+        }
+      }
+    /**
+    * 跳转到设置-允许安装未知来源-页面
+    */
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void startInstallPermissionSettingActivity() {
+      //注意这个是8.0新API
+      Intent intent = new Intent(Settings.ACTION_MANAGE_UNKNOWN_APP_SOURCES);
+      intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+      mContext.startActivity(intent);
     }
 
     public static void install(Context context, File file, boolean force) {
